8391dc17ac0e5c43511c71eddf99a93df7b2b3df
Matt Nichols <matt@mattnichols.net>
2013-04-27 11:51:19 -0400

    better history - ignore deleted lists

--- js/history.js
+++ js/history.js
@@ -13,19 +13,26 @@ function loadAllHistory() {
 	
 	for (list in lists) {
 		var name = lists[list];
+		// create new elements
 		var $newEl = $($("#hist_template").html());
+		var $newBr = $("<br/>");
 		$newEl.attr('id', name).attr('href', name);
-		$("#history_list").append($newEl).append("<br/>");
+		$newBr.attr('class', name);
+		// attach to DOM and load title
+		$("#history_list").append($newEl).append($newBr);
 		loadName(name, loadHistoryItem);
 	}
 };
 
 function loadHistoryItem(snapshot) {
+	// load in title to proper element
 	var val = snapshot.child('title').val();
 	var $el = $("#" + snapshot.name());
 	if (val != null) $el.text(val);
-	else $el.remove();
-	// TODO: also remove the <br>
-	// TODO: also handle no lists visible (no loaded or all removed)
+	else { // list has been deleted somehow
+		$el.remove();
+		$("." + snapshot.name()).remove();
+	}
+	// fade in if necessary
 	if (++loaded == toLoad) $("#contents_wrapper").fadeIn(250);
 };
--- js/load.js
+++ js/load.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	var recent = getMostRecentList();
 	// if they've been before, load most recent list
 	if (recent != null) {
+		// check to make sure it hasn't been deleted!
 		window.location = recent;
 	} else {
 		// display welcome screen once everything is loaded
